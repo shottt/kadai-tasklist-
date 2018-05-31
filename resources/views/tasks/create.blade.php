617b38650ec156db0a19b8f3147fa7512a322ad8
EC2 Default User <ec2-user@ip-172-31-40-188.us-east-2.compute.internal>
2018-05-31 16:13:36 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,23 @@
 @section('content')
 
 <h1>タスク新規作成ページ</h1>
-
+<div class="row">
+    <div class="col-xs-12 col-sm-ofset-2 col-sm-8 col-md-ofset-2 col-md-8 col-lg-ofset-3 col-lg-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
+    
+      <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+      </div>
+      
+      <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+      </div>
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-
+    </div>
+</div>
 @endsection
